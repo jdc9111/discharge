ebd41b69a25dbececbbbfe236c276beeeb30c4a6
jdc9111 <52749095+jdc9111@users.noreply.github.com>
2020-10-23 09:30:41 -0400

    Removes refferal and time

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ console.log('it works');
 
 const printBtn = document.querySelector('.enter');
 const printDiv = document.querySelector('.printButton');
-const referralSentTo = document.querySelector('.referralSentDrop');
-const removeReferralDropDown = document.querySelector('.referralDropDown');
-const backupReferral = document.querySelector('.altReferral');
+
+const referralSentTo = document.querySelector('.referralTo');
+const referralDrop = document.querySelector('.referralDropDown');
+const aptDateTime = document.querySelector('.apts');
+
 const aptDay = document.querySelector('.aptDate');
 
 const ifAvailableDate=document.querySelector('dateHelp');
 const ifAvailableTime=document.querySelector('timeHelp');
 
 const lacerationBox=document.querySelector('#laceration');
+const headInjuryBox=document.querySelector('#headInjury');
+
 
 const instructionBox = document.getElementById("specialInstructions");
-
-console.log(printBtn);
-console.log(referralSentTo.value);
-console.log(removeReferralDropDown);
+const proceduralBox = document.querySelector('#proceduralSedation');
 
 var specialInstructions = "";
 
@@ -28,48 +29,49 @@ function addtxt() {
 */
 
 
-
-
-
-
-
 function handlePrint() {
     console.log('it got clicked');
-    console.log(referralSentTo);
-    console.log(referralSentTo.value);
-
+    
     var textBox =  document.getElementById("specialInstructions").value;
-    console.log(textBox);
-
-
+    
     if(lacerationBox.checked){
         console.log(lacerationBox.checked);
         textBox = textBox + "\nKeep you wound clean and dry.  Return to the ED if you have more pain, swelling, redness, fever, discharge from the wound or any concerns.";
-        console.log(textBox);
+        console.log(textBox);    
     };
 
-    
+    if(proceduralBox.checked){
+        textBox = textBox + "\nDo not to drive, operate heavy machinery, use hot tubs, make important decisions, sign legal documents for 24h.  Return to the ER if you have fever, vomiting, or are unable to tolerate fluids.";
+    };
+
+    if(headInjuryBox.checked){
+        textBox = textBox + "\nReturn to the ED if you headache becomes worse, you develop numbness, weakness, difficulty with speech or vision or you have have any concerns.  Follow up with your primary provider if you symptoms continue.";
+
+        document.getElementById("qr").innerHTML = `
+        <h4 class="font-weight-bold ml-3">Scan Me with your phone camera for more information:</h4>
+
+        <p>This is a test</p>
+        <img class="logo" src="/logos/concussionqr.png"/>
+        `;
+    };
+
 
     document.getElementById("specialInstructions").value=textBox;
 
-    /*
-    addtxt ();
 
-
-    
         
     /*remove 'referralDropDown if blank" */
 
     let dropDownValue = referralSentTo.value;
-    console.log(referralSentTo.value)
+    console.log(!!dropDownValue)
  
-    
-    if (dropDownValue === 'none') {
-        removeReferralDropDown.classList.add('invisible');
-        console.log('should have removed it');
-    } else {
-        backupReferral.classList.add('invisible');
+
+    if(!dropDownValue){
+        referralDrop.classList.add('invisible');
+        aptDateTime.classList.add('invisible');       
     };
+
+
 
 
     /*remove the appointment if there is none */
@@ -78,8 +80,9 @@ function handlePrint() {
     let isApt = aptDay.value;
     console.log(isApt);
 
-    if (isApt === NaN) {
+    if (!isApt) {
         console.log('remove the appointment');
+        aptDateTime.classList.add('invisible');
     };
     
     /* remove "print button" */
@@ -97,44 +100,14 @@ function handlePrint() {
 
 
 
-
-
-
     /* print the handout */
-    window.print();
+    setTimeout(() => {
+        window.print();
+    }, 500)
+
 
 };
 
 
 
 printBtn.addEventListener('click', handlePrint);
-
-
-/*
-lacerationBox.addEventListener('change',function(e){
-    if(lacerationBox.checked){
-        specialInstructions = specialInstructions + ". Keep you wound clean";
-    }
-});
-
-
-*/
-
-
-
-
-/*
-const notes = document.querySelectorAll('.note');
-    console.log(notes);
-
-    const noteArr = [];
-
-    notes.forEach(function(note) {
-        noteArr.push(note.value);
-    })
-
-    const note = noteArr.join("\n");
-    console.log(note)
-*/
-
-
