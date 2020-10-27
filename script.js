3f3a826410e721202207dcac9769970fc45e8236
jdc9111 <52749095+jdc9111@users.noreply.github.com>
2020-10-27 10:22:23 -0400

    Disappearing PDFs

--- script.js
+++ script.js
@@ -145,18 +145,18 @@ printBtn.addEventListener('click', handlePrint);
 lacerationBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
+    var lacerationInstructions="Keep your wound clean and dry.  Return to the Emergency Department if you have more pain, swelling, redness, fever, 'red streaks', discharge from the wound or any concerns.\n";
 
     if(this.checked) {
         console.log('checked');
-        
-        textBox = textBox + "\nKeep you wound clean and dry.  Return to the ED if you have more pain, swelling, redness, fever, discharge from the wound or any concerns.";
 
+        textBox = textBox + lacerationInstructions
         document.getElementById("specialInstructions").value=textBox;
 
     } else {
         console.log('unchecked');
         
-        var newTextBox = textBox.replace("\nKeep you wound clean and dry.  Return to the ED if you have more pain, swelling, redness, fever, discharge from the wound or any concerns.", "");
+        var newTextBox = textBox.replace(lacerationInstructions, "");
         document.getElementById("specialInstructions").value=newTextBox;
     }
 });
@@ -164,7 +164,7 @@ lacerationBox.addEventListener( 'change', function() {
 diverticulitisBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
-    var diverticulitisInstructions = "Take antibiotics and pain medicine as prescribed.\nStart with only clear liquids for a few days. Examples of items allowed on a clear liquid diet include: broth, fruit juices without pulp, such as apple juice, ice chips, Ice pops without bits of fruit or fruit pulp, gelatin, water, tea or coffee without cream.  As you start feeling better slowly add low-fiber foods. Examples of low-fiber foods include: canned or cooked fruits without skin or seeds, canned or cooked vegetables such as green beans, carrots and potatoes (without the skin), eggs, fish and poultry, refined white bread, fruit and vegetable juice with no pulp. low-fiber cereals, milk, yogurt and cheese, white rice, pasta and noodles. You should feel better within two or three days of starting the diet and antibiotics. If you haven't started feeling better by then, call your doctor. \nContact your doctor or return to the Emergency Department if: you develop a fever, your abdominal pain is worsening, or you are unable to keep clear liquids down.  These may indicate a complication that requires hospitalization.\nhttps://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/diverticulitis-diet/art-20048499";
+    var diverticulitisInstructions = "Take antibiotics and pain medicine as prescribed.\n\nStart with only clear liquids for a few days. Examples of items allowed on a clear liquid diet include: broth, fruit juices without pulp, such as apple juice, ice chips, Ice pops without bits of fruit or fruit pulp, gelatin, water, tea or coffee without cream.\n\nAs you start feeling better slowly add low-fiber foods. Examples of low-fiber foods include: canned or cooked fruits without skin or seeds, canned or cooked vegetables such as green beans, carrots and potatoes (without the skin), eggs, fish and poultry, refined white bread, fruit and vegetable juice with no pulp. low-fiber cereals, milk, yogurt and cheese, white rice, pasta and noodles. You should feel better within two or three days of starting the diet and antibiotics. If you haven't started feeling better by then, call your doctor. \n\nContact your doctor or return to the Emergency Department if: you develop a fever, your abdominal pain is worsening, or you are unable to keep clear liquids down.  These may indicate a complication that requires hospitalization.\nhttps://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/diverticulitis-diet/art-20048499";
 
 
     if(this.checked) {
@@ -174,18 +174,36 @@ diverticulitisBox.addEventListener( 'change', function() {
 
         document.getElementById("specialInstructions").value=textBox;
 
+        document.getElementById("qr").innerHTML = `
+
+
+        <div class="pdf">
+            <iframe src="/pdfs/lowfiberdiet.pdf" width="100%" height="400px"></iframe><br><br>
+
+            <iframe src="/pdfs/diverticulosis.pdf" width="100%" height="400px"></iframe>
+
+        </div>
+
+    `;
+
+
+
     } else {
         console.log('unchecked');
         
         var newTextBox = textBox.replace(diverticulitisInstructions, "");
         document.getElementById("specialInstructions").value=newTextBox;
+
+        document.getElementById("qr").innerHTML = ``;
+
+
     }
 });
 
 incidentalBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
-    var incidentalInstructions = "\nYou have been provided with printed copy of your imaging (X-ray, CT-scan, Ultrasound) test report. Please take it to your family MD or primary provider for review. It may contain incidental findings that require further investingation.";
+    var incidentalInstructions = "You have been provided with printed copy of your imaging (X-ray, CT-scan, Ultrasound) test report. Please take it to your family MD or primary provider for review. It may contain incidental findings that require further investigation.\n";
 
     if(this.checked) {
         console.log('checked');
@@ -194,18 +212,21 @@ incidentalBox.addEventListener( 'change', function() {
 
         document.getElementById("specialInstructions").value=textBox;
 
+
     } else {
         console.log('unchecked');
         
         var newTextBox = textBox.replace(incidentalInstructions, "");
         document.getElementById("specialInstructions").value=newTextBox;
+
+
     }
 });
 
 pediatricBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
-    var pediatricInstructions = "\nBring your child back to the Emergency Department if s/he has persistent vomiting, is not drinking or urinates less than 3 times in 24 hours, is having difficulty breathing, is unusually sleepy, has a stiff neck or complains of neck pain, is confused, is 'getting worse' or has not improved in 48-72h ";
+    var pediatricInstructions = "Bring your child back to the Emergency Department if s/he has persistent vomiting, is not drinking or urinates less than 3 times in 24 hours, is having difficulty breathing, is unusually sleepy, has a stiff neck or complains of neck pain, is confused, is 'getting worse' or has not improved in 48-72h.\n";
 
     if(this.checked) {
         console.log('checked');
@@ -225,7 +246,7 @@ pediatricBox.addEventListener( 'change', function() {
 concussionBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
-    var concussionInstructions = "\nReturn to the ED if your headache becomes worse, you experience confusion, you develop numbness, weakness, difficulty with speech or vision or you have have any concerns.  Follow up with your primary provider if you symptoms continue.";
+    var concussionInstructions = "Return to the ED if your headache becomes worse, you experience confusion, you develop numbness, weakness, difficulty with speech or vision or you have have any concerns.  Follow up with your primary provider if you symptoms continue.\n";
 
     if(this.checked) {
         console.log('checked');
@@ -240,7 +261,9 @@ concussionBox.addEventListener( 'change', function() {
 
         <img class="logo pb-5" src="/logos/concussionqr.png"/>
 
-        <iframe src="/pdfs/ConcussionDoDont.pdf" width="100%" height="500px"></iframe>
+        <div class="pdf">
+            <iframe src="/pdfs/ConcussionDoDont.pdf" width="100%" height="500px"></iframe>
+        </div>
 
     `;
 
@@ -258,7 +281,7 @@ concussionBox.addEventListener( 'change', function() {
 shinglesBox.addEventListener( 'change', function() {
 
     var textBox =  document.getElementById("specialInstructions").value;
-    var shinglesInstructions = "\nAnyone who has recovered from chickenpox, and even children, can get shingles.  Symptoms of shingles include burning or shooting pain, tingling or itching, chills, fever, headache, upset stomach, and rashes or blisters that develop on one side of the body, usually on your face or around your waist. There are medicines that may help. Healthy adults age 50 and older should talk to their healthcare professional about getting the shingles vaccine to reduce their risk. Generally shingles is not contagious, but a person with active shingles can spread the virus when the rash is in the blister phase. It's important to keep the rash covered. Most cases of shingles last 3-5 weeks. Most people get shingles only one time, but, it is possible to have it more than once.  Return to the Emergency Department if you develop worsening pain, fever, discharge from the rash, pain or redness in your eye.\n\nYou can find more information here: https://www.nia.nih.gov/health/shingles";
+    var shinglesInstructions = "Anyone who has recovered from chickenpox, even children, can get shingles.  Symptoms of shingles include burning or shooting pain, tingling or itching, chills, fever, headache, upset stomach, and rashes or blisters that develop on one side of the body, usually on your face or around your waist. There are medicines that may help. Healthy adults age 50 and older should talk to their healthcare professional about getting the shingles vaccine to reduce their risk. Generally shingles is not contagious, but a person with active shingles can spread the virus when the rash is in the blister phase. It's important to keep the rash covered. Most cases of shingles last 3-5 weeks. Most people get shingles only one time, but, it is possible to have it more than once.  Return to the Emergency Department if you develop worsening pain, fever, discharge from the rash, pain or redness in your eye.\n\nYou can find more information here: https://www.nia.nih.gov/health/shingles\n";
     
     if(this.checked) {
         console.log('checked');
